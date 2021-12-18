7804fcb211d98cd317379ba96caadbe4ecacbec8
Kristi Zentner <kristi.zentner17@gmail.com>
2021-12-19 01:17:51 +0545

    Week 1 Workshop Assignment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,7 +10,10 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
